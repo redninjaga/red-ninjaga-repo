51d685a6e1e4d3bd96a610e589078818fa42a038
Red Ninjaga <redninjaga@gmail.com>
2024-06-10 20:58:28 +0300

    Add homework_15 part 1

--- hillel/homework/homework_15.py
+++ hillel/homework/homework_15.py
@@ -3,12 +3,6 @@
 path_read_file = 'files/examples.txt'
 path_write_file = 'files/result.txt'
 
-
-def calculate(line: str) -> int:
-    """Написать функцию которая решает пример"""
-    return 0
-
-
 with open(path_read_file, 'r', encoding='utf-8') as file:
     lines = file.readlines()
     clean_lines = []
@@ -16,16 +10,11 @@ with open(path_read_file, 'r', encoding='utf-8') as file:
         line = line.replace('\n', '')
         line = line.strip()
         clean_lines.append(line)
-    print(clean_lines)
 
 result_list = []
 for line in clean_lines:
-    result_list.append(f'{line} = {calculate(line)}')
-print(result_list)
-
+    result_list.append(f'{line} = {eval(line)}')
 
 with open(path_write_file, 'w', encoding='utf-8') as file:
     for line in result_list:
         file.write(f"{line}\n")
-
-
